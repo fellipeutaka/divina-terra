19f5d2c40e1db7cd45ac326313691b17bf071dc0
fellipeutaka <fellipeutaka@gmail.com>
2022-12-09 22:49:11 -0300

    improve: html metatags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="pt-BR" className="scroll-smooth">
       <head>
+        <meta charSet="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Divina Terra Atibaia</title>
       </head>
       <body>{children}</body>
